22e662cb2f8af60a08350b23711f1335704efd74
Jan Kavalír <jan.kavalir@icloud.com>
2025-02-26 21:50:56 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace InvoicingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
